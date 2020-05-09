c5e6a07bdf0438c4c2f3a80c79f2326866f016c7
eam-00 <estebanmontagut@hotmail.com>
2020-05-09 17:59:38 -0300

    Added get-sysinfo.sh

---
+++ get-sysinfo.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+HOSTNAME=`which hostname`
+MY_ECHO='/usr/bin/echo -e'
+SEPARATOR='/usr/bin/echo "----------------------" '
+MY_CAT=`which cat`
+MY_UNAME=`which uname`
+MY_FREE=`which free`
+MY_DF=`which df`
+MY_IFCONFIG=`which ifconfig`
+LOG='/tmp/sysinfo.log'
+
+
+$MY_ECHO "Hostname:" > $LOG
+$HOSTNAME >> $LOG
+$SEPARATOR >> $LOG
+
+$MY_ECHO >> $LOG
+
+if [ -f "/etc/lsb-release" ]; then
+$MY_ECHO "LSB Release:" >> $LOG
+$MY_CAT /etc/lsb-release >> $LOG
+fi
+$SEPARATOR >> $LOG
+
+$MY_ECHO >> $LOG
+
+if [ -f "/etc/os-release" ]; then
+$MY_ECHO "OS Release:" >> $LOG
+$MY_CAT /etc/os-release >> $LOG
+$SEPARATOR >> $LOG
+fi
+
+$MY_ECHO >> $LOG
+
+if [ -f "/etc/debian_version" ]; then
+$MY_ECHO "Debian Version:" >> $LOG
+$MY_CAT /etc/debian_version >> $LOG
+$SEPARATOR >> $LOG
+fi
+
+$MY_ECHO >> $LOG
+
+$MY_ECHO "Kernel Version:" >> $LOG
+$MY_UNAME -a >> $LOG
+$SEPARATOR >> $LOG
+$MY_ECHO >> $LOG
+
+$MY_ECHO >> $LOG
+
+if [ -f "/proc/cpuinfo" ]; then
+$MY_ECHO "CPU Info:" >> $LOG
+$MY_CAT /proc/cpuinfo >> $LOG
+$SEPARATOR >> $LOG
+fi
+
+$MY_ECHO "RAM Info:" >> $LOG
+$MY_FREE -m >> $LOG
+$SEPARATOR >> $LOG
+
+$MY_ECHO "HDD Space Info:" >> $LOG
+$MY_DF -h >> $LOG
+$SEPARATOR >> $LOG
+
+if [ -f "/sbin/ifconfig" ]; then
+$MY_ECHO "ifconfig Info:" >> $LOG
+$MY_IFCONFIG >> $LOG
+$SEPARATOR >> $LOG
+fi
+
+# ip a >> /tmp/server-info.txt
+# cat /etc/network/interfaces >> /tmp/server-info.txt
+# netstat -nr >> /tmp/server-info.txt
+# Snips from /etc/rsnapshot.conf
+# cat /usr/local/etc/scripts/backups/tar-backups.sh >> /tmp/server-info.txt
+
+## EoF ##
+
