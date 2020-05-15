2f61c521a767ab83ec4ca83fc7a635e956617164
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-15 20:13:37 -0300

    Added reboot-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -8,6 +8,7 @@ apt-get install logwatch zile monit mailutils rsnapshot
 
 mkdir -p /var/cache/logwatch
 mkdir -p /root/Zile/Backups/
+mkdir -p /usr/local/etc/scripts/security/
 mkdir -p /home/$REGULAR_USER/Zile/Backups/
 
 cp /etc/logwatch/conf/logwatch.conf /etc/logwatch/conf/logwatch.conf.ORIG
@@ -23,6 +24,7 @@ cp .zile $REGULAR_USER/
 cp logrotate.conf /etc/logrotate.conf
 cp sshd_config /etc/ssh/sshd_config
 cp logwatch.conf /etc/logwatch/conf/logwatch.conf
+cp reboot-email.sh /home/$REGULAR_USER/Zile/Backups/
 
 chown -R $REGULAR_USER:$REGULAR_USER /home/$REGULAR_USER/Zile/Backups/
 chmod 0700 /home/$REGULAR_USER
