a0256e84b51fe64dfb61fd9867abc4cd796936c5
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-15 18:55:09 -0300

    Added mkdirs for the Zile/Backups

--- setup-server.sh
+++ setup-server.sh
@@ -7,6 +7,9 @@ REGULAT_USER=''
 apt-get install logwatch zile monit mailutils rsnapshot
 
 mkdir -p /var/cache/logwatch
+mkdir -p /root/Zile/Backups/
+mkdir -p /home/$REGULAT_USER/Zile/Backups/
+
 cp /etc/logwatch/conf/logwatch.conf /etc/logwatch/conf/logwatch.conf.ORIG
 
 cp /etc/logrotate.conf /etc/logrotate.conf.ORIG
@@ -15,7 +18,7 @@ cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
 cd /tmp/
 
 cp .zile /root/
-cp .zile $REGULAT_USER
+cp .zile $REGULAT_USER/
 
 cp logrotate.conf /etc/logrotate.conf
 cp sshd_config /etc/ssh/sshd_config
