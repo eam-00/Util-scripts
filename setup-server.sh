e779be5450180880b2b96c17b2da27f067f7fdee
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-15 18:33:18 -0300

    Added setup-server.sh

---
+++ setup-server.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rsync {}
+
+mkdir -p /var/cache/logwatch
+cp /etc/logwatch/conf/logwatch.conf /etc/logwatch/conf/logwatch.conf.ORIG
+
+cp /etc/logrotate.conf /etc/logrotate.conf.ORIG
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
+
+cd /tmp/
+
+cp logrotate.conf /etc/logrotate.conf
+cp sshd_config /etc/ssh/sshd_config
+cp logwatch.conf /etc/logwatch/conf/logwatch.conf
+
+## EoF ##
