2812aa58a4b05c6e3f5ae3eedc4bf5c729c9c622
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-17 19:35:04 -0300

    Added path to chmod, chown & mkdir

--- setup-server.sh
+++ setup-server.sh
@@ -6,22 +6,22 @@ REGULAR_USER=''
 
 apt-get install logwatch zile monit mailutils rsnapshot
 
-chown -R $REGULAR_USER:$REGULAR_USER /home/$REGULAR_USER/
-chmod 0700 /home/$REGULAR_USER
+/usr/bin/chown -R $REGULAR_USER:$REGULAR_USER /home/$REGULAR_USER/
+/usr/bin/chmod 0700 /home/$REGULAR_USER
 
 
 ## Logwatch
-mkdir -p /var/cache/logwatch
+/usr/bin/mkdir -p /var/cache/logwatch
 cp /etc/logwatch/conf/logwatch.conf /etc/logwatch/conf/logwatch.conf.ORIG
 cp /tmp/setup-server/logwatch.conf /etc/logwatch/conf/logwatch.conf
 
 
-mkdir -p /root/Zile/Backups/
-mkdir -p /home/$REGULAR_USER/Zile/Backups/
+/usr/bin/mkdir -p /root/Zile/Backups/
+/usr/bin/mkdir -p /home/$REGULAR_USER/Zile/Backups/
 cp /tmp/.zile /root/
 cp /tmp/.zile /home/$REGULAR_USER/
 
-mkdir -p /usr/local/etc/scripts/security/
+/usr/bin/mkdir -p /usr/local/etc/scripts/security/
 
 cp /etc/logrotate.conf /etc/logrotate.conf.ORIG
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
