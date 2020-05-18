a22765116ff80fc225ff8404d469fcbf197d4aa6
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-18 19:50:10 -0300

    More comments and clarification

--- setup-server.sh
+++ setup-server.sh
@@ -5,14 +5,15 @@
 REGULAR_USER=''
 SETUP_FILES_DIR=''
 
-apt-get install logwatch zile monit mailutils rsnapshot
 
-## Logwatch
+/usr/bin/apt-get install logwatch zile monit mailutils rsnapshot
+
+## Logwatch setup
 /usr/bin/mkdir -p /var/cache/logwatch
 cp /etc/logwatch/conf/logwatch.conf /etc/logwatch/conf/logwatch.conf.ORIG
 cp /$SETUP_FILES_DIR/logwatch.conf /etc/logwatch/conf/logwatch.conf
 
-
+## Zile setup
 /usr/bin/mkdir -p /root/Zile/Backups/
 /usr/bin/mkdir -p /home/$REGULAR_USER/Zile/Backups/
 cp /$SETUP_FILES_DIR/.zile /root/
@@ -22,17 +23,18 @@ cp /$SETUP_FILES_DIR/.zile /home/$REGULAR_USER/
 /usr/bin/chown -R $REGULAR_USER:$REGULAR_USER /home/$REGULAR_USER/
 /usr/bin/chmod 0700 /home/$REGULAR_USER
 
+
 ## Email Reboot Notification
 /usr/bin/mkdir -p /usr/local/etc/scripts/security/
 cp /$SETUP_FILES_DIR/reboot-email.sh /usr/local/etc/scripts/security/
 
-## Backup first
+## Logrotate
 cp /etc/logrotate.conf /etc/logrotate.conf.ORIG
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
-
 cp /$SETUP_FILES_DIR/logrotate.conf /etc/logrotate.conf
-cp /$SETUP_FILES_DIR/sshd_config /etc/ssh/sshd_config
 
+## SSHD
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
+cp /$SETUP_FILES_DIR/sshd_config /etc/ssh/sshd_config
 
 
 ## EoF ##
