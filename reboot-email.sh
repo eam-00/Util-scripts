ad17e57096fc0fc57db54525a273fc2950ec40b8
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-24 18:01:03 -0300

    Corrected an extra space on the DEST line

--- reboot-email.sh
+++ reboot-email.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-## Send an email upon server reboot
+## Sends an email upon server reboot
 ## This script has to be invoked thru a cronjob when the server reboots:
 ## @reboot /bin/sh /path/to/reboot-email.sh 1>/tmp/_debug.reboot-email.1.log 2>/tmp/_debug.reboot-email.2.log
 
 ## Multiple destinataries has to be separated by a space
-DEST='xxxxxxx@xxxxxxx.net xxxxx@xxxx.com '
+DEST='xxxxxxx@xxxxxxx.net xxxxx@xxxx.com'
 HOSTNAME=`/bin/hostname`
 MAIL=`which mail`
 CAT=`which cat`
