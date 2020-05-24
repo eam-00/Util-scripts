4503da2ddec4fb027d437dc84f30ac39ba7eb00b
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-05-24 20:16:14 -0300

    Added genMon-time.sh and the Genmon directory

---
+++ Genmon/genMon-time.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## genMon-time.sh
+## Prints the time, using a specific format: "17:23"
+## There is a special character that prints an FontAwesome Clock right at the start of the date.
+## FontAwesome Cheat Sheet: https://fontawesome.com/cheatsheet
+
+echo "<txt>" "| " $(date +"%H:%M")"</txt>"
+echo "<tool>""</tool>"
+
+## EoF ##
