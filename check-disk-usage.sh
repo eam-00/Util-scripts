bae1f3bacbb9577a7d7bea164e93391ea86f6afb
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-08-04 21:47:26 -0300

    Set MY_TAIL

--- check-disk-usage.sh
+++ check-disk-usage.sh
@@ -3,11 +3,11 @@
 DISK='/dev/mapper/optiplex--755--usfm--vg-root'
 PERCENTAGE='94'
 DF='/bin/df'
-TAIL='/usr/bin/tail'
+MY_TAIL='/usr/bin/tail -1'
 AWK='/usr/bin/awk'
 TR='/usr/bin/tr'
 
-if [ $($DF --output=pcent $DISK | $TAIL -1 | $AWK '{print $1}' | $TR -cd '0-9\n') -gt $PERCENTAGE ]; then
+if [ $($DF --output=pcent $DISK | $MY_TAIL | $AWK '{print $1}' | $TR -cd '0-9\n') -gt $PERCENTAGE ]; then
  echo "Usa mucho espacio de HDD"
 else
  echo "No usa mucho espacio de HDD"
