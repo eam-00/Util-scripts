8b3c0c22aec91bf5699beccee4104f55da9c1db3
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-08-05 00:05:08 -0300

    Set the exit status messages

--- check-disk-usage.sh
+++ check-disk-usage.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 DISK='/dev/mapper/t60p--vg-root'
-PERCENTAGE='54'
+PERCENTAGE='56'
 MY_DF='/bin/df --output=pcent'
 MY_TAIL='/usr/bin/tail -1'
-AWK='/usr/bin/awk'
+MY_AWK='/usr/bin/awk'
 MY_TR='/usr/bin/tr -cd '0-9\n''
 
-if [ $($MY_DF $DISK | $MY_TAIL | $AWK '{print $1}' | $MY_TR) -gt $PERCENTAGE ]; then
- echo "Usa mucho espacio de HDD"
+if [ $($MY_DF $DISK | $MY_TAIL | $MY_AWK '{print $1}' | $MY_TR) -gt $PERCENTAGE ]; then
+ echo "Root Partition: Check Available Disk Space"
  exit 1
 else
- echo "No usa mucho espacio de HDD"
+ echo "Root Partition: Available Disk Space Is Normal"
  exit 0
 fi
 
