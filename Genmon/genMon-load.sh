4ff525384851bdb75591c5f0f7c00ba0e2267b1a
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-08-18 11:12:26 -0300

    Added the font options, bold and color

--- Genmon/genMon-load.sh
+++ Genmon/genMon-load.sh
@@ -3,7 +3,13 @@
 ## genMon-load.sh
 ## Prints the first value of the CPU load average, also a FontAwesome icon right before the load info
 
-echo "<txt>" " |  " $(cat /proc/loadavg | awk '{print $1}') "</txt>"
+## echo "<txt>" " |  " $(cat /proc/loadavg | awk '{print $1}') "</txt>"
+
+## Adapta fgcolor color and bold font
+## echo "<txt><span weight=\"bold\" fgcolor=\"#11eded\">" " |  " $(cat /proc/loadavg | awk '{print $1}') "</span></txt>"
+
+## Bold font using the font color provided by the theme:
+echo "<txt><span weight=\"bold\">" " |  " $(cat /proc/loadavg | awk '{print $1}') "</span></txt>"
 echo "<tool>""</tool>"
 
 ## EoF ##
