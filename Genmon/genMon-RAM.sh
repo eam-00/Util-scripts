c03e6d8b3d13291da5fa4efb45088693e9167860
Epsilon Alpha Mu <estebanmontagut@hotmail.com>
2020-08-24 13:49:40 -0300

    Added the font options, bold and color

--- Genmon/genMon-RAM.sh
+++ Genmon/genMon-RAM.sh
@@ -3,7 +3,14 @@
 ## genmon-RAM.sh
 ## Prints the percentage of RAM usage, also a FontAwesome icon right before the information
 
-echo "<txt>" "|  " $(free -m | awk 'NR==2{printf "RAM: %s/%sMB %.2f%%\n", $3,$2,$3*100/$2 }'| cut -d " " -f3)"</txt>"
+## No bold text:
+## echo "<txt>" "|  " $(free -m | awk 'NR==2{printf "RAM: %s/%sMB %.2f%%\n", $3,$2,$3*100/$2 }'| cut -d " " -f3)"</txt>"
+
+## Adapta fgcolor color and bold font
+## echo "<txt><span weight=\"bold\" fgcolor=\"#11eded\">""| " $(free -m | awk 'NR==2{printf "RAM: %s/%sMB %.2f%%\n", $3,$2,$3*100/$2 }'| cut -d " " -f3)"</span></txt>"
+
+## Bold font using the font color provided by the theme:
+echo "<txt><span weight=\"bold\">""| " $(free -m | awk 'NR==2{printf "RAM: %s/%sMB %.2f%%\n", $3,$2,$3*100/$2 }'| cut -d " " -f3)"</span></txt>"
 echo "<tool>""</tool>"
 
 ## EoF ##
