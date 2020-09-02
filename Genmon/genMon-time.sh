c10c8bf931142defe3f5a4dd9c5bddd0eac9cb82
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-09-02 15:33:58 -0300

    Update genMon-time.sh

--- Genmon/genMon-time.sh
+++ Genmon/genMon-time.sh
@@ -5,7 +5,13 @@
 ## There is a special character that prints a FontAwesome Clock right in front of the hour.
 ## FontAwesome Cheat Sheet: https://fontawesome.com/cheatsheet
 
-echo "<txt>" "| " $(date +"%H:%M")"</txt>"
+## echo "<txt>" "| " $(date +"%H:%M")"</txt>"
+
+## An Adapta color of sorts
+## echo "<txt><span weight=\"bold\" fgcolor=\"#11eded\">" "|  " $(date +"%H:%M")"</span></txt>"
+
+## No color (uses the one defined by the theme) & bold font:
+echo "<txt><span weight=\"bold\">" "|  " $(date +"%H:%M")"</span></txt>"
 echo "<tool>""</tool>"
 
 ## EoF ##
