4a50fbbd08d0f7cd17da65fc75a8e527ad47c567
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-09-04 14:04:39 -0300

    Comment and the evaluation for terminal

--- Genmon/genMon-date.sh
+++ Genmon/genMon-date.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash                                                                                                                                            
                                                                                                                                                                
 ## genMon-date.sh                                                                                                                                              
-## Prints the date, using a specific format: "Tuesday 17 Sep 2019"                                                                                             
+## Prints the date, using a specific format: "Tuesday 17 Sep 2019",
+## also when clicked, it pops a calendar with the current month displayed.
                                                                                                                                                                
 echo "<txt>" "|  " $(date +"%A %d %b %Y")"</txt>"                                                                                                              
 echo "<tool>""</tool>"                                                                                                                                         
-## echo "<txtclick>"xterm -hold -e /usr/bin/cal"</txtclick>"                                                                                                   
+if command -v xterm > /dev/null 2>&1; then                                                                                                                     
+echo "<txtclick>"xterm -hold -e /usr/bin/cal"</txtclick>"                                                                                                      
+else                                                                                                                                                           
 echo "<txtclick>"xfce4-terminal --hold --command=/usr/bin/cal"</txtclick>"                                                                                     
+fi                                                                                                                                                             
                                                                                                                                                                
 ## EoF ##                                                                                                                                                      
