ce4a103205458759c2c8f50095553d3dc2372c28
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-09-04 15:03:27 -0300

    Added bold font and AwesomeFont Calendar

--- Genmon/genMon-date.sh
+++ Genmon/genMon-date.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash                                                                                                                                            
-                                                                                                                                                               
-## genMon-date.sh                                                                                                                                              
+#!/usr/bin/env bash
+## genMon-date.sh
 ## Prints the date, using a specific format: "Tuesday 17 Sep 2019",
 ## also when clicked, it pops a calendar with the current month displayed.
+
+echo "<txt><span weight=\"bold\">" "|  "  $(date +"%A %d %b %Y")"</span></txt>"
+echo "<tool>""</tool>"
+if command -v xterm > /dev/null 2>&1; then
+echo "<txtclick>"xterm -hold -e /usr/bin/cal"</txtclick>"
+else
+echo "<txtclick>"xfce4-terminal --hold --command=/usr/bin/cal"</txtclick>"
+fi
                                                                                                                                                                
-echo "<txt>" "|  " $(date +"%A %d %b %Y")"</txt>"                                                                                                              
-echo "<tool>""</tool>"                                                                                                                                         
-if command -v xterm > /dev/null 2>&1; then                                                                                                                     
-echo "<txtclick>"xterm -hold -e /usr/bin/cal"</txtclick>"                                                                                                      
-else                                                                                                                                                           
-echo "<txtclick>"xfce4-terminal --hold --command=/usr/bin/cal"</txtclick>"                                                                                     
-fi                                                                                                                                                             
-                                                                                                                                                               
-## EoF ##                                                                                                                                                      
+## EoF ##
