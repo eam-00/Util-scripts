e6bd0101509de76d2329ca7f807decc7c5a7f3c4
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-09-06 21:54:33 -0300

    Added bold font

--- Genmon/genMon-cpu-temp.sh
+++ Genmon/genMon-cpu-temp.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 cat /sys/class/thermal/thermal_zone0/temp > /tmp/cpu_temp
-## echo $((`cat /tmp/cpu_temp`/1000))"°"
-echo "<txt>"" | "$((`cat /tmp/cpu_temp`/1000))"°""</txt>"
+echo "<txt><span weight=\"bold\">"" | "$((`cat /tmp/cpu_temp`/1000))"°""</span></txt>"
 echo "<tool>""</tool>"
 
 ## EoF ##
