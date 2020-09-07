e9e9ebadbe27cea3840e557438f8a281722d3de2
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-09-07 00:03:51 -0300

    Added comment with name and description

--- Genmon/genMon-cpu-temp.sh
+++ Genmon/genMon-cpu-temp.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+## genMon-cpu-temp.sh
+## Prints the temperature on the CPU.
+
 cat /sys/class/thermal/thermal_zone0/temp > /tmp/cpu_temp
 echo "<txt><span weight=\"bold\">"" | "$((`cat /tmp/cpu_temp`/1000))"°""</span></txt>"
 echo "<tool>""</tool>"
