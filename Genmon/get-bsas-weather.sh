cc1e754cfd98b9b9c8de8dd18ecf21740db73b3c
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-09-09 13:26:32 -0300

    Added get-bsas-weather.sh

---
+++ Genmon/get-bsas-weather.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## More info on WTTR.in: https://github.com/chubin/wttr.in
+
+WGET='/usr/bin/wget -q -O-'
+FULL_FORMAT='http://v2.wttr.in/aep'
+SHORT_FORMAT='http://v2.wttr.in/aep?format=3'
+LOG='/tmp/aep.tmp'
+
+## Full format (lots of information):
+$WGET $FULL_FORMAT > $LOG
+
+## Short format:
+## If you use this format, be sure to edit the file $HOME/Local/Scripts/genmon/genMon-bsas-weather.sh
+## $WGET $SHORT_FORMAT > $LOG
+
+#############
+## Cronjob ##
+#############
+## @reboot /bin/bash /home/epsilon-alpha-mu/Local/Scripts/genmon/get-bsas-weather.sh
+## */19 * * * * /bin/bash /home/epsilon-alpha-mu/Local/Scripts/genmon/get-bsas-weather.sh
+
+## EoF ##
