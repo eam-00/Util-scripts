ca67ebb4f807c85a7c1714ecfd428f753693818b
eam-00 <64646896+eam-00@users.noreply.github.com>
2021-07-11 02:06:44 -0300

    Added if to eval color based on load

--- Genmon/genMon-load.sh
+++ Genmon/genMon-load.sh
@@ -4,14 +4,16 @@
 ## Prints the first value of the CPU load average, also a FontAwesome icon right before the load info
 ## Genmon plugin configuration: Period (s): 28,50
 
-## echo "<txt>" " |  " $(cat /proc/loadavg | awk '{print $1}') "</txt>"
-
+LOAD=$(cat /proc/loadavg | awk '{print $1}')
+if [ $LOAD \> 1.10 ]
+then
 ## Adapta fgcolor color and bold font
-## echo "<txt><span weight=\"bold\" fgcolor=\"#11eded\">" " |  " $(cat /proc/loadavg | awk '{print $1}') "</span></txt>"
-
-## Bold font using the font color provided by the theme:
-echo "<txt><span weight=\"bold\">" " |  " $(cat /proc/loadavg | awk '{print $1}') "</span></txt>"
-echo "<tool>""</tool>"
+    echo "<txt><span weight=\"bold\" fgcolor=\"#11eded\">" " |  " $LOAD "</span></txt>"
+    echo "<tool>""</tool>"
+else
+    echo "<txt><span weight=\"bold\">" " |  " $LOAD "</span></txt>"
+    echo "<tool>""</tool>"
+fi
 
 ## EoF ##
 
