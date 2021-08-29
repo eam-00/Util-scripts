0bf55d8b0ca9c0218b1d2499b279bcfce046076b
eam-00 <64646896+eam-00@users.noreply.github.com>
2021-08-29 17:16:25 -0300

    Added Eval LOG in case of problems with wttr.in

--- Genmon/genMon-bsas-weather.sh
+++ Genmon/genMon-bsas-weather.sh
@@ -5,10 +5,18 @@
 ## This script uses the output from the execution of the script:
 ## ~/Local/Scripts/genmon/get-bsas-weather.sh
 
-CAT="/usr/bin/cat"
+CAT="/bin/cat"
 LOG="/tmp/aep.tmp"
+WTTR_ERROR="Unknown location; please try ~-34.5592566,-58.4146741723315"
 GREP="/usr/bin/grep"
 CUT="/usr/bin/cut"
+
+## Eval the LOG contents in case there are problems with wttr.in
+if [ cat /tmp/aep.tmp = $WTTR_ERROR ]
+ then echo "No Value" > /tmp/LOG_ERROR
+  echo "<txt>" "|  " $($CAT /tmp/LOG_ERROR)"</txt>"
+  echo "<tool>""</tool>"
+ else
 
 ## With the weather condition glyph:
 ## cat /tmp/aep.tmp | grep -e 'Weather:' | cut -d' ' -f2-
@@ -21,12 +29,12 @@ CUT="/usr/bin/cut"
 ## echo "<txt><span weight=\"bold\" fgcolor=\"#11eded\">" "|  " $($CAT $LOG | $GREP -e 'Weather:' | $CUT -d' ' -f3-)"</span></txt>"
 
 ## No color, bold font:
-echo "<txt><span weight=\"bold\">" "|  " $($CAT $LOG | $GREP -e 'Weather:' | $CUT -d' ' -f3-)"</span></txt>"
+## echo "<txt><span weight=\"bold\">" "|  " $($CAT $LOG | $GREP -e 'Weather:' | $CUT -d' ' -f3-)"</span></txt>"
 
 ## The one to be used with the SHORT_FORMAT
-## echo "<txt>" "|  " $($CAT $LOG)"</txt>"
+echo "<txt>" "|  " $($CAT $LOG)"</txt>"
 
 echo "<tool>""</tool>"
+fi
 
 ## EoF ##
-
