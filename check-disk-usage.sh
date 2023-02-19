2eadb973838c1ba8acb708524e70a5e0ecf91ddb
eam-00 <64646896+eam-00@users.noreply.github.com>
2023-02-18 22:23:34 -0300

    Set which command

--- check-disk-usage.sh
+++ check-disk-usage.sh
@@ -9,7 +9,7 @@ DISK='/dev/mapper/t60p--vg-root'
 PERCENTAGE='56'
 MY_DF='/bin/df --output=pcent'
 MY_TAIL='/usr/bin/tail -1'
-MY_AWK='/usr/bin/awk'
+MY_AWK=$(which awk)
 MY_TR='/usr/bin/tr -cd '0-9\n''
 
 if [ $($MY_DF $DISK | $MY_TAIL | $MY_AWK '{print $1}' | $MY_TR) -gt $PERCENTAGE ]; then
