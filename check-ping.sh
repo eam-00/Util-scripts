a6ab731a84b1fdebde888384e496137201c7e098
eam-00 <64646896+eam-00@users.noreply.github.com>
2023-03-07 23:52:28 -0300

    1

---
+++ check-ping.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# This is intended for hosts availability checking. 
+
+# Variables
+hosts="${1}"
+cmd_ping=$(which ping)
+cmd_nslookup=$(which nslookup)
+
+
+# Clear the screen
+clear
+
+
+# Check if the ARG is present
+if [ -z "${1}" ]; then
+        echo
+        echo -e "You must provide the host/s [ARG1]"
+        echo
+        echo "Usage:"
+        echo -e "\t$0 \"hostname1\""
+        echo -e "\t\tor "
+        echo -e "\t$0 \"hostname1 hostname2 hostname2 ...\""
+        echo
+        exit
+fi
+
+
+# Print one dividing line.
+echo "----------------------------"
+
+
+# Main
+for host in ${hosts}; do
+        # Ping the hosts
+        $cmd_ping -c10 $host &>/dev/null
+        if [ $? -eq 0 ]; then
+                echo "Status for ${host}: UP" 
+
+
+##                ip_nslookup=$(${cmd_nslookup} ${host} | grep -i address: | tail -1 | awk '{print $2}')
+##                if [ ! -z ${ip_nslookup} ]; then
+##                        echo "IP Address for ${host}: ${ip_nslookup}"
+##                else
+##                        echo "IP Address for ${host}: DNS not resolving"
+##                fi
+        else
+                echo "$host: DOWN"
+        fi
+        echo "----------------------------"
+done
+
+# EOF #
