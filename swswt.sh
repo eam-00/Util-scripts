0bc07ff334204bd40d00ce4db33b4e5457da10ab
eam-00 <64646896+eam-00@users.noreply.github.com>
2023-03-12 22:59:45 -0300

    8

--- swswt.sh
+++ swswt.sh
@@ -5,6 +5,8 @@ cmd_wmctrl=$(which wmctrl)
 
 # Switch to Virtual Desktop number 4
 $cmd_wmctrl -s 3
+
+## Set the specified theme
 xfconf-query -c xfwm4 -p /general/theme -s Numix-compact
 
 ## EOF ##
