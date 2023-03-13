11936ce7204ecd9e30a3d7aa42fbefe40f495eaf
eam-00 <64646896+eam-00@users.noreply.github.com>
2023-03-12 22:57:07 -0300

    5

--- swswt.sh
+++ swswt.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ## Switch Workspace Switch Window Theme
 
+# Switch to Virtual Desktop number 4
 cmd_wmctrl=$(which wmctrl) -s 3
 xfconf-query -c xfwm4 -p /general/theme -s Numix-compact
 
