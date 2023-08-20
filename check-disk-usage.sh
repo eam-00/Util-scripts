39b1ba0ea6d9847301e7185838775a7c65112afa
eam-00 <64646896+eam-00@users.noreply.github.com>
2023-08-20 17:34:09 -0300

    Edit link to Monit

--- check-disk-usage.sh
+++ check-disk-usage.sh
@@ -3,7 +3,7 @@
 ## check-disk-usage.sh
 ## Checks a disk or partition and if the usage goes above the
 ## PERCENTAGE it prints a warning.
-## Used to make checks via [Monit](https://github.com/eam-00/Monit/tree/master) 
+## Used to make checks via Monit - https://github.com/eam-00/Monit/
 
 DISK='/dev/mapper/t60p--vg-root'
 PERCENTAGE='56'
